31653bea69655df7aaa29bce7594a74f183e2394
am607 <akbarcp607@gmail.com>
2022-04-20 13:37:02 +0530

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+//add two number
+function add(a,b){
+    return a+b;
+}
+//subtract two number
+function subtract(a,b){ 
+    return a-b;
+}
+//multiply two number
+function multiply(a,b){
+    return a*b;
+}   
+//divide two number 
